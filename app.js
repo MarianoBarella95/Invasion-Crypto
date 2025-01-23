060622679e36c60e4569e9c9da0c40d26cb4dc73
MarianoBarella95 <marianobarella1@gmail.com>
2025-01-22 21:06:44 -0300

    Correcciones para Invasión y modificaciones en páginas secundarias

--- app.js
+++ app.js
@@ -1,27 +1,13 @@
-// Selecciona los logos
-const logos = document.querySelectorAll('.logo');
-const container = document.querySelector('.banner');
+// SWIPER JS
 
-// Crea la animación al hacer hover
-container.addEventListener('mouseenter', () => {
-  logos.forEach((logo) => {
-    const randomX = Math.random() * 200 - 100; // Valores aleatorios
-    const randomY = Math.random() * 200 - 100;
-    gsap.to(logo, {
-      x: randomX,
-      y: randomY,
-      rotation: Math.random() * 360,
-      duration: 0.5,
-    });
-  });
-});
-
-// Vuelve a su posición original
-container.addEventListener('mouseleave', () => {
-  gsap.to(logos, {
-    x: 0,
-    y: 0,
-    rotation: 0,
-    duration: 0.5,
-  });
+const swiper = new Swiper('.swiper', {
+  loop: true, // Permite un bucle infinito
+  autoplay: {
+    delay: 1500, // Cambia de diapositiva cada 3 segundos
+    disableOnInteraction: false, // Continúa el autoplay incluso si el usuario interactúa
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
